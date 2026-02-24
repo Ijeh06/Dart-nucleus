ac05d481baeca4cfbb3e47a29964177649dcd1f4
Ekene Ijeh <ijehekene404@gmail.com>
2026-02-24 17:13:28 +0100

    Add example Dart file demonstrating variable declaration and type promotion

---
+++ variables_and_data_types/bin/example12.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  final double hisAge = 10;
+  print(
+    hisAge,
+  ); // using the double variable to typ promote it to an int variable
+
+  //   final int herAge = 12.4; // this will cause an error because we cannot assign a double value to an int variable
+  //   print(herAge);
+}
