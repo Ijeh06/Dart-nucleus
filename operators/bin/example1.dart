cef5c8b88f82a0d6389489aba622be6fd0b7bc4b
Ekene Ijeh <ijehekene404@gmail.com>
2026-02-25 15:05:39 +0100

    Add initial project files including README, CHANGELOG, and analysis options

---
+++ operators/bin/example1.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  const age1 = 64;
+  const age2 = 30;
+  print(age1 + age2); // addition
+  print(age1 + age2 + 30);
+  print(age1 - age2); // subtraction
+  print(age1 * age2); // multiplication
+  print(age1 / age2); // division
+  const double divided = age1 / age2;
+  print(divided);
+  const int divided2 = age1 ~/ age2; // integer division
+  print(divided2);
+}
